222a29548a77d03e505f777c02d6c3d59d1efb68
Stanislaw Jagiella <Stanislaw.Jagiella@gmail.com>
2019-09-21 15:59:18 +0200

    Initial commit

---
+++ src/percentage-gauge/percentage-gauge.tsx
@@ -0,0 +1,42 @@
+import React, {FunctionComponent} from 'react'
+import './percentage-guage.css'
+
+export const linearGradient = (color: string, value: number): string => {
+    return `linear-gradient(to right, ${color}, transparent ${value}%)`
+};
+
+export type TPercentageGaugeProps = {
+    value: number | null;
+    color?: string;
+}
+
+export const PercentageGauge = (props: TPercentageGaugeProps) => {
+
+    const {
+        value,
+        color = 'blue'
+    } = props;
+
+    const percentageNumber = value !== null ? value : 0;
+    const percentageStr = `${percentageNumber}%`;
+
+    return (
+        <div style={{background: linearGradient(color, percentageNumber)}}
+             className={"percentage-guage"}>{percentageStr}
+        </div>
+    )
+};
+
+
+type TA = {
+    n: number;
+    s: string;
+    fun_b: (k: number) => boolean
+}
+
+
+const a = {
+    n: 10,
+    s: 'test',
+    fun_b: (x: number) => x > 10 ? true : false
+};
